85ac2684f7d191deda1f8a467df326dcfeda8773
fabricio022 <fabriciosp360@gmail.com>
2024-08-14 11:51:20 -0300

    exercicios

---
+++ 2024.1/ilp/exercicios.py
@@ -0,0 +1,124 @@
+#n = int (input('Digite um número: '))
+#t = n*1, n*2, n*3, n*4, n*5, n*6, n*7, n*8, n*9, n*10
+#print('A tabuada deste número é: ',t)
+
+#n1 = int(input('Digite um número: '))
+#n2 = int(input('Digite um número: '))
+#r1 = n1+n2
+#r2 = n1-n2
+#r3 = n1*n2
+#r4 = n1/n2
+#print('A soma desses valores é: ',r1)
+#print('A subtração desses valores é: ',r2)
+#print('A multiplicação desses valores é: ',r3)
+#print('A divisão desses valores é: ',r4)
+
+#temp = float(input('Digite a tempertura em Graus CELSIUS: '))
+#r = (temp*9/5) + 32
+#print ('A temperatura em Fahrenheit é: ',r)
+
+#r = int(input("Digite o valor do raio do círculo: "))
+#a = 3.14*r**2
+#print('A área deste círculo é: ',a)
+
+#b = int(input("Digite o valor da base do triângulo: "))
+#h = int(input("Digite o valor da altura do triângulo: "))
+#a = b*h/2
+#print("O valor da área do Triângulo é: ",a)
+
+#r = float(input("Digite o valor do raio da Esfera: "))
+#a = 4/3*3.14**3
+#print('O volume da Esfera é: ',a)
+
+#n1 = float(input('Digite um valor: '))
+#n2 = float(input('Digite um valor: '))
+#n3 = float(input('Digite um valor: '))
+#m = (n1+n2+n3)/3
+#print('A média entre esses valores é: {:.2f}'.format(m))
+
+#s = int(input('Digite um número de Segundos: '))
+#v1 = (s/60)
+#v2 = (s/60)/60
+#print('O número aprentado em minutos é de: {}. E o número apresentado em horas é de: {}'.format(v1,v2))
+
+#a = int(input('Digite o valor de A:'))
+#b = int(input('Digite o valor de B:'))
+#c = int(input('Digite o valor de C:'))
+#x = int(input('Digite o valor de X:'))
+#y = a**2+b*x+c
+#print("O valor de Y é: ",y)
+
+#peso = float(input('Digite seu peso: '))
+#altura = float(input('Digite sua altura: '))
+#imc = peso/altura**2
+#print('Seu IMC é ', imc)
+
+#salario = float(input('Digite o valor do seu Salário:'))
+#reajuste = float(input('Digite a porcentagem do reajuste: '))
+#resultado = (salario*reajuste)+salario
+#print('Seu novo salário é: R$',resultado)
+
+#real = float(input('Digite o seu dinheiro em Dólares: '))
+#dolar = real*4.98
+#print('Seu dinheiro em reais equivale a: {:.2f} '.format(dolar))
+
+#import math
+#num = int(input('Digite um número: '))
+#raiz = math.sqrt(num)
+#print('A raiz de {} é igual a {:.2f}'.format(num, math.floor(raiz)))
+
+#import random
+#n1 = str (input('Digite o nome do primeiro participante: '))
+#n2 = str (input('Digite o nome do segundo participante: '))
+#n3 = str (input('Digite o nome do terceiro participante: '))
+#lista = [n1,n2,n3]
+#random.choice(lista)
+#print('O PARTICIPANTE SORTEADO FOI: ', lista)
+
+
+#import random
+#n1 = str (input('Digite o nome do primeiro participante: '))
+#n2 = str (input('Digite o nome do segundo participante: '))
+#n3 = str (input('Digite o nome do terceiro participante: '))
+#lista = [n1,n2,n3]
+#random.shuffle(lista)
+#print(A ORDEM SORTEADA FOI: ', lista)
+
+
+#import math
+#n = float (input(" DIGIE UM VALOR REAL: "))
+#result = math.floor(n)
+#print(result)
+
+
+#ca = float(input('Digite o comprimento do cateto adjacente: '))
+#co = float(input('Digite o comprimento do cateto oposto: '))
+#hip = (ca**2+co**2) ** (1/2)
+#print('O comprimento da hipotenusa é: {:.2f}'.format(hip))
+
+#dias = float(input('Digite quantos dias você ficou com o carro: '))
+#km = float(input('Digite os Km rodados: '))
+#preco = (dias*60)+(km*0.15)
+#print('O PREÇO FINAL DO ALUGUEL SERÁ: R$ ', preco)
+
+
+#tempo = int(input('Digite quantos anos tem seu carro:'))
+#if tempo <=5:
+#    print('Seu carro pode ser utilizado para uber!')
+#else: 
+#    print('Seu carro não pode ser usado na Uber!')    
+
+#vl = int(input('Digite a velocidade atingida pelo automóvel: '))
+#taxa = (vl-80)*7
+#mut = (vl-80)
+#if vl > 80:
+#    print('Você ultrapassoou o limite de velocidade em {} Km/h. Será multado no valor de R${}.'.format(mut,taxa))
+#else:
+#    print('A velocidade está dentro da permitida.')
+
+
+#print("ESCOLHA O DIA DE SUA VISITA ")
+#data = int(input('Digite o dia do mês: '))
+##fech = (2,9,16,23,30):
+ #if data == fech:
+  #   print('Nesse dia estaremos fechados :(')
